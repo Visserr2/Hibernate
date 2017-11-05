5c4d10b328441e06f5f296c0acc07168c2a289c6
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2017-11-05 16:26:11 +0100

    Verified db connection
    Verified db connection

---
+++ src/test/java/nl/thuis/tutorial/jdbc/TestJdbc.java
@@ -0,0 +1,28 @@
+package nl.thuis.tutorial.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class TestJdbc {
+
+	private static final String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+	private static final String user = "hbstudent";
+	private static final String pass = "hbstudent";
+
+	public static void main(String[] args) {
+
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+			System.out.println("Connection successful!!!");
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+
+	}
+
+}
