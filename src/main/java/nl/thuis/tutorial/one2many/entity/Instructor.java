5a9dc31c8bcdcef2838b9465e5fa43920f9ca930
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2017-11-25 20:49:51 +0100

    Added classes for OneToMany Relationships

---
+++ src/main/java/nl/thuis/tutorial/one2many/entity/Instructor.java
@@ -0,0 +1,106 @@
+package nl.thuis.tutorial.one2many.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="instructor")
+public class Instructor {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	private String email;
+	
+	// create relationship with InstructorDetail
+	// CascadeType.PERSIST : means that save() or persist() operations cascade to related entities.
+    // CascadeType.MERGE : means that related entities are merged when the owning entity is merged.
+    // CascadeType.REFRESH : does the same thing for the refresh() operation.
+    // CascadeType.REMOVE : removes all related entities association with this setting when the owning entity is deleted.
+    // CascadeType.DETACH : detaches all related entities if a “manual detach” occurs.
+    // CascadeType.ALL : is shorthand for all of the above cascade operations.
+	// Use multiple types: cascade= {CascadeType.PERSIST, CascadeType.MERGE, etc}
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="instructor_detail_id") // refer to foreign key
+	private InstructorDetail instructorDetail;
+	// Never use cascade type remove in a ManytoOne or OneToMany relationship
+	@OneToMany(mappedBy="instructor", cascade={CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+	private List<Course> courseList;
+	
+	public Instructor() {}
+
+	public Instructor(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public InstructorDetail getInstructorDetail() {
+		return instructorDetail;
+	}
+	public void setInstructorDetail(InstructorDetail instructorDetail) {
+		this.instructorDetail = instructorDetail;
+	}
+	public List<Course> getCourseList() {
+		return courseList;
+	}
+	public void setCourseList(List<Course> courseList) {
+		this.courseList = courseList;
+	}
+	
+	// Convenience method for adding course to instructor and setting relationship by adding instructor to course
+	public void addCourse(Course course) {
+		if(courseList == null) {
+			courseList = new ArrayList<Course>();
+		}
+		
+		courseList.add(course);
+		course.setInstructor(this);
+	}
+
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", instructorDetail=" + instructorDetail + "]";
+	};	
+}
---
+++ src/main/java/nl/thuis/tutorial/one2many/entity/InstructorDetail.java
@@ -0,0 +1,70 @@
+package nl.thuis.tutorial.one2many.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column(name="youtube_channel")
+	private String youtubeChannel;
+	private String hobby;	
+	// Make the OneToOne relationship bidirectional. The instructor is loaded in the instructor_detail too now.
+	// Refers to foreign key of the other class
+	// CascadeType.PERSIST : means that save() or persist() operations cascade to related entities.
+    // CascadeType.MERGE : means that related entities are merged when the owning entity is merged.
+    // CascadeType.REFRESH : does the same thing for the refresh() operation.
+    // CascadeType.REMOVE : removes all related entities association with this setting when the owning entity is deleted.
+    // CascadeType.DETACH : detaches all related entities if a “manual detach” occurs.
+    // CascadeType.ALL : is shorthand for all of the above cascade operations.
+	// Use multiple types: cascade= {CascadeType.PERSIST, CascadeType.MERGE, etc}
+	@OneToOne(mappedBy="instructorDetail", cascade=CascadeType.ALL)
+	private Instructor instructor;
+	
+	public InstructorDetail() {}
+	
+	public InstructorDetail(String youtubeChannel, String hobby) {
+		this.youtubeChannel = youtubeChannel;
+		this.hobby = hobby;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getYoutubeChannel() {
+		return youtubeChannel;
+	}
+	public void setYoutubeChannel(String youtubeChannel) {
+		this.youtubeChannel = youtubeChannel;
+	}
+	public String getHobby() {
+		return hobby;
+	}
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	public Instructor getInstructor() {
+		return instructor;
+	}
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
+	}	
+}
