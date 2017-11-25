4ca5eaba866138dbbe0b36cde062c35bfdc9b9cd
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2017-11-25 13:47:52 +0100

    Added OneToOne Create And Delete Demo
    Added OneToOne Create And Delete Demo

---
+++ src/main/java/nl/thuis/tutorial/one2one/CreateOneToOneDemo.java
@@ -0,0 +1,47 @@
+package nl.thuis.tutorial.one2one;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import nl.thuis.tutorial.one2one.entity.Instructor;
+import nl.thuis.tutorial.one2one.entity.InstructorDetail;
+
+public class CreateOneToOneDemo {
+
+	public static void main(String[] args) {
+		// Creating sessionfactory
+				SessionFactory sessionFactory = new Configuration()
+													.configure("hibernate-one2one.cfg.xml")
+													.addAnnotatedClass(Instructor.class)
+													.addAnnotatedClass(InstructorDetail.class)
+													.buildSessionFactory();
+
+				// get session
+				Session session = sessionFactory.getCurrentSession();
+				
+				try {
+					// Creating objects
+					Instructor instructor = new Instructor("Ronald", "Visser", "Ronald.Visser@hotmail.nl");
+					InstructorDetail instructorDetail = new InstructorDetail("https://youtube.com", "Youtube");
+					// Associating objects
+					instructor.setInstructorDetail(instructorDetail);
+					
+					// Begin Transaction
+					session.beginTransaction();
+					
+					// Save instructor - this also saves InstructorDetail because of cascade all
+					session.save(instructor);					
+					
+					// Commit transaction
+					session.getTransaction().commit();
+					
+				} catch(Exception e) {
+					e.printStackTrace();
+				} finally {
+					// close session and session factory
+					session.close();
+					sessionFactory.close();
+				}
+	}
+}
---
+++ src/main/java/nl/thuis/tutorial/one2one/DeleteOneToOneDemo.java
@@ -0,0 +1,45 @@
+package nl.thuis.tutorial.one2one;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import nl.thuis.tutorial.one2one.entity.Instructor;
+import nl.thuis.tutorial.one2one.entity.InstructorDetail;
+
+public class DeleteOneToOneDemo {
+
+	public static void main(String[] args) {
+		// Creating sessionfactory
+				SessionFactory sessionFactory = new Configuration()
+													.configure("hibernate-one2one.cfg.xml")
+													.addAnnotatedClass(Instructor.class)
+													.addAnnotatedClass(InstructorDetail.class)
+													.buildSessionFactory();
+
+				// get session
+				Session session = sessionFactory.getCurrentSession();
+				
+				try {					
+					// Begin Transaction
+					session.beginTransaction();
+					
+					// get instructor - null if object cannot be found
+					int id = 1;
+					Instructor instructor = session.get(Instructor.class, id);
+					
+					// Delete instructor and instructordetail because of cascade all
+					session.delete(instructor);
+					
+					// Commit transaction
+					session.getTransaction().commit();
+					
+				} catch(Exception e) {
+					e.printStackTrace();
+				} finally {
+					// close session and session factory
+					session.close();
+					sessionFactory.close();
+				}
+	}
+}
